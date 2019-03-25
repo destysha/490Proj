0bd4656e6ea38d77dd6ea82cb32620a3d6e9b483
et75 <et75@njit.edu>
2019-03-25 18:00:36 -0400

    notification function added

--- funtionFiles/functions.inc.php
+++ funtionFiles/functions.inc.php
@@ -27,5 +27,37 @@ function deleteRow($businessID, $grp_id){
 
 } 
 
+function notification($email $product){
+	global $db;
+	$pull2 = "SELECT * FROM inventory";
+	$pull = "SELECT * FROM json";
+	$val = mysqli_query($ishopdb, $pull, $pull2) or die (mysqli_error($ishopdb));
+	$num = mysqli_num_row($val);
+	while ($r = mysqli_feth_array($val, MYSQLI_ASSOC)){
+		$pd	= $r['product_description'];
+		$pdn	= $r['product'];
+		
+		$pc	= $r['postal_code'];
+		$zip	= $r['zipcode'];
+		
+		$rf	= $r['recalling_firm'];
+		$br	= $r['brand'];
+
+		if($zip == $pc){
+			if($pdn == $pd){
+				if($br == $rf){
+					return 1;
+				}
+			}
+			
+			else{
+				return 0;
+			}
+		}
+		else{
+			return 0;
+		}
+	}
+}
 
 ?>

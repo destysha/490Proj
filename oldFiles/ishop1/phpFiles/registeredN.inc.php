81a351ad5d3f3aae33a3955b3c5d6abaa613539a
Shaiddy Perez <sp2235@njit.edu>
2019-03-26 05:14:51 -0400

    styling modification

--- oldFiles/ishop1/phpFiles/registeredN.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $myfile = fopen("log.txt","a+") or die ("file can not open");
-        $today = date("l jS \of F Y h:i:s A");
-	$fail = "Account Already Exists with";
-        fwrite($myfile,"** ".$today."\n".$fail." User:".$logs[0]."\n");
-        fcloser($myfile);       
-?>
-

846c3510650e442bc4929a4accbb17b68b549ee1
Shaiddy Perez <43626646+destysha@users.noreply.github.com>
2019-03-26 10:55:33 -0400

    Update businessInv.php

--- userInterface/businessInv.php
+++ userInterface/businessInv.php
@@ -30,7 +30,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title> <?php echo $bzname ?> | Inventory </title>
+    <title> <?php echo $bzname; ?> | Inventory </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/main.css">
   <!--===============================================================================================-->

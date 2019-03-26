6469abd131248e49d23c70ac5ead57b88628419b
et75 <et75@njit.edu>
2019-03-25 23:28:23 -0400

    edited Notification Functions

--- rabbitMQFiles/Server2.php
+++ rabbitMQFiles/Server2.php
@@ -21,40 +21,6 @@ function doRegister($username,$bzname,$street,$city,$state,$zipcode,$email,$pass
 	return $register->validateRegister($username,$bzname,$street,$city,$state,$zipcode,$email,$password);
 }
 
-function notification($email, $product){
-        global $db;
-        $pull2 = "SELECT * FROM inventory";
-        $pull = "SELECT * FROM json";
-        $val = mysqli_query($ishopdb, $pull, $pull2) or die (mysqli_error($ishopdb));
-        $num = mysqli_num_row($val);
-        while ($r = mysqli_feth_array($val, MYSQLI_ASSOC)){
-                $pd     = $r['product_description'];
-                $pdn    = $r['product'];
-
-                $pc     = $r['postal_code'];
-                $zip    = $r['zipcode'];
-
-                $rf     = $r['recalling_firm'];
-                $br     = $r['brand'];
-
-                if($zip == $pc){
-                        if($pdn == $pd){
-                                if($br == $rf){
-                                        return 1;
-                                }
-                        }
-
-                        else{
-                                return 0;
-                        }
-                }
-                else{
-                        return 0;
-                }
-        }
-}
-
-
 function requestProcessor($request)
 {
   	echo "received request".PHP_EOL;
